6d522966eb1badf13052a28f76a5593a674057d9
nodirbekrajaboff <nodirbekrajaboff@gmail.com>
2025-01-07 12:43:58 +0500

    Initialize Vue 3 project with Vite, Tailwind CSS, and basic structure

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <main>
+    <section class="relative">
+      <div class="container">
+        <div
+          class="text-white flex flex-col items-start max-w-[600px] w-full pt-[200px] pb-[277px] relative z-10"
+        >
+          <span class="mb-8 text-base text-primary uppercase"
+            >Architecture design
+          </span>
+          <h1 class="mb-3 text-7xl font-normal uppercase">
+            Professional Interior Design
+          </h1>
+          <p class="mb-12 text-gray text-xl font-normal">
+            Design for now with the awareness of yesterday and unknown tomorrow.
+            Because small details make the perfect design. Helping you choose
+            between good and best.
+          </p>
+          <a
+            href=""
+            class="uppercase font-medium py-4 px-8 border text-white text-base"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+      <span
+        class="absolute bottom-0 left-0 text-dark/40 font-extrabold text-9xl uppercase"
+      >
+        Interior
+      </span>
+      <img src="/images/path.svg" alt="Path" class="absolute top-0 left-0" />
+      <img
+        class="absolute top-0 right-0 w-1/2 h-full object-cover"
+        src="/images/hero-img.png"
+        alt="Hero background"
+      />
+    </section>
+    <section class="py-[140px]">
+      <div class="container">
+        <h3
+          class="text-center mb-3 uppercase text-primary text-base font-normal"
+        >
+          top services
+        </h3>
+        <h2 class="capitalize text-center text-white font-normal text-6xl">
+          Our Specializations
+        </h2>
+        <div class="mt-12 grid grid-cols-4 gap-8">
+          <div class="relative -top-[130px] overflow-hidden rounded-2xl">
+            <img src="/images/image1.png" alt="Image" />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">Floor plan</h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div class="relative overflow-hidden rounded-2xl">
+            <img src="/images/image2.png" alt="Image" />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">
+                Interior Design
+              </h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              </p>
+            </div>
+          </div>
+          <div class="relative overflow-hidden rounded-2xl">
+            <img src="/images/image3.png" alt="Image" />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">
+                Architecture Design
+              </h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div class="relative -top-[130px] overflow-hidden rounded-2xl">
+            <img src="/images/image4.png" alt="Image" />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">Construction</h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
